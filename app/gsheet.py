7e71d5d60547e2fb28ec65abbea6565e91f9a6ce
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-03-15 14:31:27 -0700

    Update gsheet.py

--- app/gsheet.py
+++ app/gsheet.py
@@ -1,11 +1,11 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
+import json
 
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name((os.environ['GOOGLE_CREDENTIALS'], scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ['GOOGLE_CREDENTIALS'], scope)
 client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
