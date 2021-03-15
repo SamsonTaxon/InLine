62834cab08c0b4d37099748a496f1bad0a7d5dd6
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-03-14 20:31:22 -0700

    index

--- app/routes.py
+++ app/routes.py
@@ -25,31 +25,27 @@ webapp_bp = Blueprint('main', __name__)
 error_bp = Blueprint('errors', __name__)
 
 
-@webapp_bp.route('/')
-def welcome():
-    return render_template("welcome.html")
+@webapp_bp.route('/', methods=['POST', 'GET'])
+def index():
+    referring_uuid = request.args.get('user')
+    form = EmailForm()
+    if request.method == 'POST':
+        if form.validate():
+            email = utils.normalize_email(form.email.data)
+            user = logic.get_user_by_email(email)
 
-# @webapp_bp.route('/', methods=['POST', 'GET'])
-# def index():
-#     referring_uuid = request.args.get('user')
-#     form = EmailForm()
-#     if request.method == 'POST':
-#         if form.validate():
-#             email = utils.normalize_email(form.email.data)
-#             user = logic.get_user_by_email(email)
+            if user is None:
+                user = logic.create_user(email)
 
-#             if user is None:
-#                 user = logic.create_user(email)
+                myemail.send_verification_email(
+                    user.id, user.email,
+                    referring_uuid=referring_uuid)
 
-#                 myemail.send_verification_email(
-#                     user.id, user.email,
-#                     referring_uuid=referring_uuid)
+            return redirect(url_for('main.waitlist', user=user.waitlist.uuid))
 
-#             return redirect(url_for('main.waitlist', user=user.waitlist.uuid))
-
-#     elif request.method == 'GET':
-#         return render_template(
-#             'index.html', form=form, referring_uuid=referring_uuid)
+    elif request.method == 'GET':
+        return render_template(
+            'index.html', form=form, referring_uuid=referring_uuid)
 
 
 @webapp_bp.route('/verify_email/<token>/')
