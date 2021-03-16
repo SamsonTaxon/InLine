6e602ef0a91220c83c1d1e9a896f2710dd2fee43
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-03-16 09:01:38 -0700

    d

--- app/routes.py
+++ app/routes.py
@@ -29,48 +29,8 @@ error_bp = Blueprint('errors', __name__)
 
 @webapp_bp.route('/', methods=['POST', 'GET'])
 def index():
-    # referring_uuid = request.args.get('user')
-    # form = EmailForm()
-    # if request.method == 'POST':
-    #     if form.validate():
-    #         email = utils.normalize_email(form.email.data)
-    #         user = logic.get_user_by_email(email)
-
-    #         if user is None:
-    #             user = logic.create_user(email)
-
-    #             myemail.send_verification_email(
-    #                 user.id, user.email,
-    #                 referring_uuid=referring_uuid)
-
-    #         return redirect(url_for('main.waitlist', user=user.waitlist.uuid))
-
-    # elif request.method == 'GET':
-    #     return render_template(
-    #         'index.html', form=form, referring_uuid=referring_uuid)
     return render_template('index.html')
 
-
-@webapp_bp.route('/verify_email/<token>/')
-def verify_email(token):
-    logic.verify_email(token)
-    return redirect(url_for('main.index'))
-
-
-@webapp_bp.route('/waitlist/')
-def waitlist():
-    uuid = request.args.get('user')
-    waitlist_user = logic.get_waitlist_user(uuid)
-    if waitlist_user is None:
-        return abort(404)
-    else:
-        waitlist_position = logic.get_waitlist_position(uuid)
-        completed_referrals = logic.get_completed_referrals(uuid)
-        return render_template(
-            'waitlist.html',
-            uuid=uuid,
-            waitlist_position=waitlist_position,
-            completed_referrals=completed_referrals)
 
 @webapp_bp.route('/line/')
 def line():
@@ -93,49 +53,6 @@ def internal_error(error):
 
 
 api = AuthyApiClient(os.environ['AUTHY_API_KEY'])
-
-
-# @webapp_bp.route("/phone_verification", methods=["GET", "POST"])
-# def phone_verification():
-#     if request.method == "POST":
-#         country_code = request.form.get("country_code")
-#         phone_number = request.form.get("phone_number")
-#         method = request.form.get("method")
-
-#         session['country_code'] = country_code
-#         session['phone_number'] = phone_number
-
-#         api.phones.verification_start(phone_number, country_code, via=method)
-
-#         return redirect(url_for('main.verify'))
-
-#     return render_template("phone_verification.html")
-
-
-# @webapp_bp.route("/verify", methods=["GET", "POST"])
-# def verify(token):
-#     if request.method == "POST":
-#             token = request.form.get("token")
-
-#             phone_number = session.get("phone_number")
-#             country_code = session.get("country_code")
-
-#             verification = api.phones.verification_check(phone_number,
-#                                                          country_code,
-#                                                          token)
-#             phoneNumber = phone_number
-#             if verification.ok(token):
-#                 return Response("<h1>Success!</h1>")
-#                 logic.verify_number(token)
-
-#                 if user is None:
-#                     user = logic.create_user_phone(phoneNumber)
-#                     user.email_confirmed = True
-#                     now = datetime.now(timezone.utc)
-#                     user.email_confirmed_on = now
-#                     user.save()
-
-#     return render_template("verify.html")
 
 @webapp_bp.route("/join/<code>", methods=["GET", "POST"])
 def phone_verification(code):
@@ -187,7 +104,5 @@ def verify():
                 gsheet.create_user(uid,phone_num,referred_by)
 
                 return redirect(url_for('main.line'))
-
-                # return render_template("line.html", uuid=uid)
 
     return render_template("verify.html")
