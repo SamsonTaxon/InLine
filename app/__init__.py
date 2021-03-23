ea1b2d39dc8b5a66625aba4fcefc1361579f42f5
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-03-23 08:35:47 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,20 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(BaseConfig)
 
-    register_extensions(app)
+    # register_extensions(app)
     register_blueprints(app)
 
     return app
 
 
-def register_extensions(app):
-    from app.extensions import db
-    from app.extensions import mail
-    from app.extensions import migrate
+# def register_extensions(app):
+#     from app.extensions import db
+#     from app.extensions import mail
+#     from app.extensions import migrate
 
-    db.init_app(app)
-    mail.init_app(app)
-    migrate.init_app(app, db)
+#     db.init_app(app)
+#     mail.init_app(app)
+#     migrate.init_app(app, db)
 
 
 def register_blueprints(app):
