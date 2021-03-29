ec40196010196482bc8101ccd2786a374e06a7ea
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-03-29 10:56:08 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,60 +8,60 @@ class BaseModel(object):
         db.session.commit()
 
 
-class User(BaseModel, db.Model):
-    __tablename__ = 'user'
+# class User(BaseModel, db.Model):
+#     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    email_confirmed = db.Column(db.Boolean, default=False)
-    email_confirmed_on = db.Column(db.DateTime(timezone=True))
-    waitlist = db.relationship('Waitlist', uselist=False, back_populates='user')
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     email_confirmed = db.Column(db.Boolean, default=False)
+#     email_confirmed_on = db.Column(db.DateTime(timezone=True))
+#     waitlist = db.relationship('Waitlist', uselist=False, back_populates='user')
 
-    def __init__(self, email):
-        self.email = email
-
-
-referrals = db.Table(
-    'referral',
-    db.Column('referring', db.String(8), db.ForeignKey('waitlist.uuid')),
-    db.Column('referred', db.String(8), db.ForeignKey('waitlist.uuid')),
-    db.PrimaryKeyConstraint('referring', 'referred', name='referrals_pk')
-)
+#     def __init__(self, email):
+#         self.email = email
 
 
-class Waitlist(BaseModel, db.Model):
-    __tablename__ = 'waitlist'
+# referrals = db.Table(
+#     'referral',
+#     db.Column('referring', db.String(8), db.ForeignKey('waitlist.uuid')),
+#     db.Column('referred', db.String(8), db.ForeignKey('waitlist.uuid')),
+#     db.PrimaryKeyConstraint('referring', 'referred', name='referrals_pk')
+# )
 
-    initial_score = 65231
-    decrease_per_referral = 10
 
-    uuid = db.Column(db.String(8), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', back_populates='waitlist')
-    score = db.Column(db.Integer)
-    referred = db.relationship(
-        'Waitlist',
-        secondary=referrals,
-        primaryjoin=(referrals.c.referring == uuid),
-        secondaryjoin=(referrals.c.referred == uuid),
-        backref=db.backref('referral', lazy='dynamic'), lazy='dynamic')
+# class Waitlist(BaseModel, db.Model):
+#     __tablename__ = 'waitlist'
 
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.set_uuid()
-        self.set_initial_score()
+#     initial_score = 65231
+#     decrease_per_referral = 10
 
-    def set_uuid(self):
-        uuid = utils.generate_simple_uuid(8)
-        while db.session.query(Waitlist).filter_by(uuid=uuid).one_or_none():
-            uuid = utils.generate_simple_uuid(8)
-        self.uuid = uuid
+#     uuid = db.Column(db.String(8), primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user = db.relationship('User', back_populates='waitlist')
+#     score = db.Column(db.Integer)
+#     referred = db.relationship(
+#         'Waitlist',
+#         secondary=referrals,
+#         primaryjoin=(referrals.c.referring == uuid),
+#         secondaryjoin=(referrals.c.referred == uuid),
+#         backref=db.backref('referral', lazy='dynamic'), lazy='dynamic')
 
-    def set_initial_score(self):
-        self.score = self.initial_score + db.session.query(Waitlist).count() + 1
+#     def __init__(self, user_id):
+#         self.user_id = user_id
+#         self.set_uuid()
+#         self.set_initial_score()
 
-class Users(BaseModel, db.Model):
-    __tablename__ = 'users'
-    uuid = db.Column(db.String(8), primary_key=True)
-    phone_number = db.Column(db.String(120))
-    referred_by = db.Column(db.String(8))
+#     def set_uuid(self):
+#         uuid = utils.generate_simple_uuid(8)
+#         while db.session.query(Waitlist).filter_by(uuid=uuid).one_or_none():
+#             uuid = utils.generate_simple_uuid(8)
+#         self.uuid = uuid
+
+#     def set_initial_score(self):
+#         self.score = self.initial_score + db.session.query(Waitlist).count() + 1
+
+# class Users(BaseModel, db.Model):
+#     __tablename__ = 'users'
+#     uuid = db.Column(db.String(8), primary_key=True)
+#     phone_number = db.Column(db.String(120))
+#     referred_by = db.Column(db.String(8))
