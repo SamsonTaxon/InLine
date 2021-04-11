f559213d0bdad161992f2efc5110e17a4bcf1f8a
Samson Taxon <samsontaxon@Samsons-MBP-2.lan1>
2021-04-10 21:09:30 -0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,9 @@ from wtforms import SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 from wtforms.validators import Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo
+
 
 # class EmailForm(FlaskForm):
 #     email = EmailField('Email', validators=[DataRequired(), Email()])
